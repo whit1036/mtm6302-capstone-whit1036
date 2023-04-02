72847823d6631054ed1c4cba211fcff5af2520a4
Brennan Whitworth <whit1036@algonquinlive.com>
2023-04-01 21:31:31 -0400

    testing api, making updates

--- script.js
+++ script.js
@@ -1,2 +1,21 @@
-const $favsButton = document.getElementById('favs-btn')
-console.log($favsButton)
+const $generateButton = document.getElementById('generate-btn')
+
+$generateButton.addEventListener('click', ()=>{
+    console.log('button pressed')
+    sendApiRequest()
+})
+
+async function sendApiRequest(){
+    let API_KEY = "rBrWOtPUsRcCbdwGdfUbAEbX9h7vDWIHdeyeOmOZ"
+    let response = await fetch(`https://api.nasa.gov/planetary/apod?api_key=${API_KEY}`);
+    console.log(response)
+    let data = await response.json()
+    console.log(data)
+    useAPIData(data)
+}
+
+function useAPIData(data) {
+    document.getElementById('exData').innerHTML += data.explanation
+    document.getElementById('dateData').innerHTML += data.date
+    document.getElementById('nameData').innerHTML += data.title
+}

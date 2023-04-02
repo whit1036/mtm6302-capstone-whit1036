6a6b8a3bc5a29c9591d7d760884516146e617450
Brennan Whitworth <whit1036@algonquinlive.com>
2023-04-01 21:43:31 -0400

    testing api

--- script.js
+++ script.js
@@ -18,4 +18,7 @@ function useAPIData(data) {
     document.getElementById('exData').innerHTML += data.explanation
     document.getElementById('dateData').innerHTML += data.date
     document.getElementById('nameData').innerHTML += data.title
+    document.getElementById('modalnameData').innerHTML += data.title
+    document.getElementById('imgData').innerHTML += `<img src="${data.url}" class="w-75" alt="Small Generated Image/Video">`
+    document.getElementById('hdimgData').innerHTML += `<img src="${data.hdurl}" class="w-100" alt="Large Generated Image/Video">`
 }

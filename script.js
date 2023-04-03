469cacf3084e6fdf884339e692d8f907744f4e84
Brennan Whitworth <whit1036@algonquinlive.com>
2023-04-02 20:03:13 -0400

    adds comments to script

--- script.js
+++ script.js
@@ -1,10 +1,13 @@
+// creates a variable to target the generate button
 const $generateButton = document.getElementById('generate-btn')
 
+// uses the addEventListener method to create a click function to the generate button
 $generateButton.addEventListener('click', ()=>{
     console.log('button pressed')
     sendApiRequest()
 })
 
+// creates an async function to test, and fetch data from the NASA APOD API
 async function sendApiRequest(){
     let API_KEY = "rBrWOtPUsRcCbdwGdfUbAEbX9h7vDWIHdeyeOmOZ"
     let response = await fetch(`https://api.nasa.gov/planetary/apod?api_key=${API_KEY}`);
@@ -14,6 +17,7 @@ async function sendApiRequest(){
     useAPIData(data)
 }
 
+// creates a function to use the data found in the API to dynamically insert API data
 function useAPIData(data) {
     document.getElementById('exData').innerHTML += data.explanation
     document.getElementById('dateData').innerHTML += data.date

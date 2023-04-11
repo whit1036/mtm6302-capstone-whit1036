132a98a8d84f26264754637932998288d2298bef
Brennan Whitworth <whit1036@algonquinlive.com>
2023-04-10 20:43:43 -0400

    updates script to display image from today

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ const $favsimgButton = document.getElementById('favsimgSave')
 // uses the addEventListener method to create a click function to the generate button
 $generateButton.addEventListener('click', ()=>{
     console.log('button pressed')
-    sendApiRequest()
+    sendApiRequest($chooseDate.value)
 })
 
 // uses the addEventListener method to create a click function to the save images to favs button
@@ -20,14 +20,17 @@ $favsimgButton.addEventListener('click', ()=>{
 })
 
 // creates an async function to test, and fetch data from the NASA APOD API
-async function sendApiRequest(){
+async function sendApiRequest(date){
     let API_KEY = "rBrWOtPUsRcCbdwGdfUbAEbX9h7vDWIHdeyeOmOZ"
-    let response = await fetch(`https://api.nasa.gov/planetary/apod?api_key=${API_KEY}&date=${$chooseDate.value}`);
+    let response = await fetch(`https://api.nasa.gov/planetary/apod?api_key=${API_KEY}&date=${date}`);
     console.log(response)
     let data = await response.json()
     console.log(data)
     useAPIData(data)
 }
+
+// Call the API with no date value to display today's picture
+sendApiRequest('')
 
 // creates a function to use the data found in the API to dynamically insert API data
 function useAPIData(data) {
